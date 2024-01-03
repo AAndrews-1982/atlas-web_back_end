ef6f8137b738f27375f2030909dab0e9a7aa66f4
“AAndrews-1982 <altonandrews@icloud.com>
2024-01-03 16:20:19 -0600

    I think I got it

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def to_str(n: float) -> str:
+    """
+    Convert a float to its string representation.
+
+    Parameters:
+    n (float): A floating-point number.
+
+    Returns:
+    str: The string representation of the floating-point number.
+    """
+    return str(n)
