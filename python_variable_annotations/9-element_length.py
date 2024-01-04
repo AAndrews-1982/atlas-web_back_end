0b45a757c7e2647d7e196f1c8b5539ff83d326e6
“AAndrews-1982 <altonandrews@icloud.com>
2024-01-04 16:52:25 -0600

    added line after line 3 & 6, shorten documentation on line 14

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """9. Let's duck type an iterable object"""
 
+
 from typing import Iterable, Sequence, List, Tuple
+
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
@@ -9,6 +11,7 @@ def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     Parameters:
     lst (Iterable[Sequence]): An iterable containing sequences.
     Returns:
-    List[Tuple[Sequence, int]]: A list of tuples, each containing a sequence and its length.
+    List[Tuple[Sequence, int]]: A list of tuples,
+    each containing a sequence and its length.
     """
     return [(i, len(i)) for i in lst]
