aa085c8c37ada2c7053f7d5fa8696cbc835ca216
“AAndrews-1982 <altonandrews@icloud.com>
2024-01-04 17:56:22 -0600

    Added another import statement

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,15 +8,16 @@ The coroutine will collect 10 random numbers
 using async comprehension over async_generator,
 then return the 10 random numbers.
 """
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
 
 
-from your_previous_module import async_generator
-# Replace with the actual module name
-
-
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     Coroutine that collects 10 random numbers using an async comprehension
     over async_generator, and then returns these numbers.
     """
-    return [i async for i in async_generator()]
+    random_numbers = [i async for i in async_generator()]
+    return random_numbers
