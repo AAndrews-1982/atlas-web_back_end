881ca937d2861664aa8b49c8a67984278ccae1e4
“AAndrews-1982 <altonandrews@icloud.com>
2024-01-04 16:44:11 -0600

    added line for line 4 and 7, shorten the length of documenation on line 10

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 """6. Complex types - mixed list"""
 
+
 from typing import List, Union
+
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Calculate the sum of a list of integers and floats.
     Parameters:
-    mxd_lst (List[Union[int, float]]): A list of integers and floating-point numbers.
+    mxd_lst (List[Union[int, float]]):
+    A list of integers and floating-point numbers.
     Returns:
     float: The sum of the elements in the list.
     """
