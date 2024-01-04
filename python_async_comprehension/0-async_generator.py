8fdead29286d89075f408bb710277f787e47158b
“AAndrews-1982 <altonandrews@icloud.com>
2024-01-04 17:49:34 -0600

    added import statement that was missing

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     Asynchronous generator coroutine that yields
     a random number between 0 and 10.
