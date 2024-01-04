21671610fbd07ee9b8f8a7e2b57988c55ab754fc
“AAndrews-1982 <altonandrews@icloud.com>
2024-01-04 16:47:31 -0600

    fixed line and documentation errors

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """7. Complex types - string and int/float to tuple"""
+
+
 from typing import Tuple, Union
+
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
@@ -9,6 +12,7 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     k (str): The string to be included in the tuple.
     v (Union[int, float]): An integer or floating-point number to be squared.
     Returns:
-    Tuple[str, float]: A tuple where the first element is k and the second element is the square of v as a float.
+    Tuple[str, float]: A tuple where the first element is
+    k and the second element is the square of v as a float.
     """
     return (k, float(v ** 2))
