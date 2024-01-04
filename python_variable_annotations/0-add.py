0c0931cbfd92ad65dd4875b5f746a47e3ed4542f
“AAndrews-1982 <altonandrews@icloud.com>
2024-01-04 12:02:05 -0600

    comments changed

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -3,12 +3,11 @@
 def add(a: float, b: float) -> float:
     """
     Add two floating point numbers.
-
     Parameters:
     a (float): The first number.
     b (float): The second number.
-
     Returns:
+
+    return a + b
     float: The sum of a and b.
     """
-    return a + b
