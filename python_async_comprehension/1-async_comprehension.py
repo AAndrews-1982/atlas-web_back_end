91a875e2f3d94d43f4c264c0c378fbc51bace0df
“AAndrews-1982 <altonandrews@icloud.com>
2024-01-04 17:53:44 -0600

    changed documentation

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
-"""1. Async Comprehensions"""
+"""1. Async Comprehensions
+
+Import async_generator from the previous task
+It then writes a coroutine called async_comprehension
+
+The coroutine will collect 10 random numbers
+using async comprehension over async_generator,
+then return the 10 random numbers.
+"""
 
 
 from your_previous_module import async_generator
