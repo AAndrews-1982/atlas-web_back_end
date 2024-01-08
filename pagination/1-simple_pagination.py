c57d5d8b7b11957e512ee28e092bf5a78b542016
“AAndrews-1982 <altonandrews@icloud.com>
2024-01-08 09:50:36 -0600

    added CSV file

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -2,10 +2,10 @@
 """1. Simple pagination"""
 
 import csv
+from typing import List
 
 
 def index_range(page: int, page_size: int) -> tuple:
-
     """
     Calculate the start and end index for a page and page size.
     """
@@ -15,10 +15,23 @@ def index_range(page: int, page_size: int) -> tuple:
 
 
 class Server:
-    def __init__(self, dataset: str):
-        self.__dataset = dataset
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> list:
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]  # Skip the header row
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
         Get a page from the dataset.
 
@@ -27,23 +40,20 @@ class Server:
         page_size (int): The number of items per page.
 
         Returns:
-        list: The list of items on the requested page.
+        List[List]: The list of items on the requested page.
         """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
+        dataset = self.dataset()
         start_index, end_index = index_range(page, page_size)
 
-        page_data = []
-        with open(self.__dataset, mode='r', encoding='utf-8') as csvfile:
-            data_reader = csv.reader(csvfile)
-            for i, row in enumerate(data_reader):
-                if i >= start_index and i < end_index:
-                    page_data.append(row)
+        if start_index >= len(dataset):
+            return []
 
-        return page_data
+        return dataset[start_index:min(end_index, len(dataset))]
 
 
 # Example usage
-server = Server('Popular_Baby_Names.csv')
+server = Server()
 print(server.get_page(1, 10))
