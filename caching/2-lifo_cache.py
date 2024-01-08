bbef7dd5c740bda184174aa1960b0ffae5934a65
“AAndrews-1982 <altonandrews@icloud.com>
2024-01-08 17:00:11 -0600

    fixed BaseCaching errors

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -15,27 +15,29 @@ class LIFOCache(BaseCaching):
         Initialize the class.
         """
         super().__init__()
-        self.last_key = None
+        self.key_order = []
 
     def put(self, key, item):
         """
         Assign to the dictionary self.cache_data
         the item value for the key key.
-        If the cache exceeds its limit, remove the last item added.
+        If the cache exceeds its limit, remove the last
+        item added.
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
-            self.last_key = key
+            if key in self.key_order:
+                self.key_order.remove(key)
+            self.key_order.append(key)
 
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                if self.last_key:
-                    del self.cache_data[self.last_key]
-                    print(f"DISCARD: {self.last_key}")
+            while len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                discarded_key = self.key_order.pop(-2)
+                # Discarding the last item before the new one
+                del self.cache_data[discarded_key]
+                print(f"DISCARD: {discarded_key}")
 
     def get(self, key):
         """
         Return the value in self.cache_data linked to key.
         """
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        return self.cache_data.get(key, None)
