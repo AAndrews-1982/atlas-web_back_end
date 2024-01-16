b29a2aae95864f946ba1162cec072d134c0b221b
“AAndrews-1982 <altonandrews@icloud.com>
2024-01-16 14:53:06 -0600

    Fixed PEP8 error

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -22,6 +22,20 @@ if AUTH_TYPE:
     auth = Auth()  # Create an instance of Auth
 
 
+@app.before_request
+def before_request_func():
+    """ Function to run before each request """
+    if auth:
+        excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                          '/api/v1/forbidden/']
+        if not auth.require_auth(request.path, excluded_paths):
+            return
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
