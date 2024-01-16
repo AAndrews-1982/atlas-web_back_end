945737fdd52b5db5ef1d3e665e5531742a5edd6e
“AAndrews-1982 <altonandrews@icloud.com>
2024-01-16 10:25:42 -0600

    0. Regex-ing

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    Obfuscates specified fields in a log message.
+
+    Args:
+    fields: A list of strings representing all fields to obfuscate.
+    redaction: A string representing by what the field will be obfuscated.
+    message: A string representing the log line.
+    separator: A string representing the character
+    separating fields in the log line.
+
+    Returns:
+    A string with the specified fields obfuscated.
+    """
+    for field in fields:
+        message = re.sub(f"{field}=[^{separator}]+",
+                         f"{field}={redaction}", message)
+    return message
