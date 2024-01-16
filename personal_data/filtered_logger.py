07f4e3152dee56ee0499c653292955e0e06778c1
“AAndrews-1982 <altonandrews@icloud.com>
2024-01-16 11:25:51 -0600

    Regex-ing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-
+"""Personal data"""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> str:
     """Obfuscate specified fields in a log message."""
     for field in fields:
-        message = re.sub(f"{field}=[^{separator}]+", 
+        message = re.sub(f"{field}=[^{separator}]+",
                          f"{field}={redaction}", message)
     return message
