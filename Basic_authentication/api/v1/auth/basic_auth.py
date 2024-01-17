136c250b220dc31e0fbbcf03fd5b273a99e55086
“AAndrews-1982 <altonandrews@icloud.com>
2024-01-17 11:06:23 -0600

    8. Basic - Base64 decode

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -26,19 +26,19 @@ class BasicAuth(Auth):
         return authorization_header[6:]
 
     def decode_base64_authorization_header(
-            self, base64_authorization_header: str) -> str:
-        """method that returns the decoded value of a Base64 string"""
-        if base64_authorization_header is None:
-            return None
-        if type(base64_authorization_header) is not str:
+            self, base64_authorization_header: str
+            ) -> str:
+        """Decodes a Base64 string to a UTF-8 string."""
+        if (base64_authorization_header is None or
+                not isinstance(base64_authorization_header, str)):
             return None
 
         try:
-            decoded_bytes = b64decode(base64_authorization_header)
-        except binascii.Error as err:
+            base64_bytes = base64_authorization_header.encode('utf-8')
+            decoded_bytes = base64.b64decode(base64_bytes)
+            return decoded_bytes.decode('utf-8')
+        except binascii.Error:
             return None
-
-        return decoded_bytes.decode("utf-8")
 
     def extract_user_credentials(
             self, decoded_base64_authorization_header: str) -> (str, str):
