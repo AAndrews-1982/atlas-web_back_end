233337719d5854ec69d08527813be838408c2cf1
“AAndrews-1982 <altonandrews@icloud.com>
2024-01-24 09:33:48 -0600

    3rd attemt to fix output errors

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -47,6 +47,6 @@ def add_user(self, email: str, hashed_password: str) -> User:
         specified email and hashed password.
     """
     new_user = User(email=email, hashed_password=hashed_password)
-    session.add(new_user)
-    session.commit()
+    self._session.add(new_user)
+    self._session.commit()
     return new_user
