c208246241df29238824121ed834eb03c1aedb59
“AAndrews-1982 <altonandrews@icloud.com>
2024-01-27 20:43:19 -0600

    output

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -87,10 +87,9 @@ class BasicAuth(Auth):
         Returns:
         - User instance or None if credentials are invalid.
         """
-        if user_email is None or \
-                not isinstance(user_email, str) or \
-                user_pwd is None or \
-                not isinstance(user_pwd, str):
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
             return None
 
         users = User.search({'email': user_email})
