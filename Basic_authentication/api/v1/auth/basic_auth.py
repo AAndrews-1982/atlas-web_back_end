3c91f6cbbd069ee99768e16f62f9dd20074888fb
“AAndrews-1982 <altonandrews@icloud.com>
2024-01-27 22:11:13 -0600

    output

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -26,12 +26,13 @@ class BasicAuth(Auth):
         Returns:
         - Base64 encoded credentials or None if invalid.
         """
-        if authorization_header is None or \
-                not isinstance(authorization_header, str) or \
-                not authorization_header.startswith('Basic '):
+        if authorization_header is None:
             return None
-
-        return authorization_header.split(' ')[1]
+        if type(authorization_header) is not str:
+            return None
+        if authorization_header[0:6] != 'Basic ':
+            return None
+        return authorization_header[6:]
 
     def decode_base64_authorization_header(
             self, base64_authorization_header: str) -> str:
@@ -89,15 +90,34 @@ class BasicAuth(Auth):
         """
         if user_email is None or not isinstance(user_email, str):
             return None
+
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
 
         users = User.search({'email': user_email})
+
         if not users:
             return None
 
-        user = users[0]
-        if not user.is_valid_password(user_pwd):
+        user_instance = users[0]
+
+        if not user_instance.is_valid_password(user_pwd):
             return None
 
-        return user
+        return user_instance
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Method that overloads Auth and retrieves the User
+        instance for a request"""
+        if request is None:
+            request = request
+
+        auth_header = self.authorization_header(request)
+        base64_auth_header = self.extract_base64_authorization_header(
+            auth_header)
+        decoded_auth_header = self.decode_base64_authorization_header(
+            base64_auth_header)
+        user_email, user_pwd = self.extract_user_credentials(
+            decoded_auth_header)
+
+        return self.user_object_from_credentials(user_email, user_pwd)
