385833d1e48e1eaf5713d52261fd37644eb26da6
“AAndrews-1982 <altonandrews@icloud.com>
2024-01-29 16:06:51 -0600

    all files added

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
+"""Unittests"""
 import unittest
+from unittest import mock
+from unittest.mock import patch
 from parameterized import parameterized
-from your_module_name import access_nested_map
-# Replace 'your_module_name' with the
-# actual name of your module
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """Test Suit AccessNestedMap."""
+    """ to test the function for following inputs """
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
-        result = access_nested_map(nested_map, path)
-        self.assertEqual(result, expected)
-
-
-if __name__ == "__main__":
-    unittest.main()
