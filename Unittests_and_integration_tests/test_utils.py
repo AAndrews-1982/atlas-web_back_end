7053757ab7b9e6babf52bdf7a22802d4b6225ae3
“AAndrews-1982 <altonandrews@icloud.com>
2024-01-29 18:43:21 -0600

    Mock HTTP calls

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -45,3 +45,23 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as raised_exception:
             access_nested_map(nested_map, path)
         self.assertEqual(str(expected_key), str(raised_exception.exception))
+
+
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """
+        Test the get_json function to ensure it returns the expected result.
+        """
+        with patch('requests.get') as mock_get:
+            mock_get.return_value = Mock(json=lambda: test_payload)
+            response = get_json(test_url)
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(response, test_payload)
+
+
+if __name__ == "__main__":
+    unittest.main()
