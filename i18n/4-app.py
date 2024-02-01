3cd17d83e3bae4c346ba5a89e73b19b5c961fecf
“AAndrews-1982 <altonandrews@icloud.com>
2024-01-31 23:43:37 -0600

    languages

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -23,30 +23,30 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+app.config.from_object('4-app.Config')
+
+
+@babel.localeselector
 def get_locale():
     """
-    Select a language translation for a request based on
-    the request's parameters or headers.
+    Determine the best matching language from supported languages.
     """
-    # Attempt to get the locale from the URL parameters
-    force_locale = request.args.get('locale')
-    if force_locale and force_locale in app.config['LANGUAGES']:
-        return force_locale
-    # Fallback to the best match from the accepted languages in the request
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        # Return the locale if it's explicitly specified in the request.
+        return locale
+    # Otherwise, return the best match from the Accept-Language header.
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def index():
+def my_home():
     """
-    The index route that renders the main page.
-
-    Returns:
-        A rendered template of '4-index.html'.
+    Render the home page of the application.
     """
-    return render_template('4-index.html')
+    return render_template('./4-index.html')
 
 
-if __name__ == '__main__':
-    # Run the app in debug mode for development purposes
-    app.run(debug=True)
+if __name__ == "__main__":
+    # This configuration makes the application accessible over the network.
+    app.run(host="0.0.0.0", port="5000")
