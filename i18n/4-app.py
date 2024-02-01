07c252aad3894330cf0e3d27c3282bbb5dc58fd8
“AAndrews-1982 <altonandrews@icloud.com>
2024-01-31 23:50:16 -0600

    Mock logging in

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,52 +1,36 @@
 #!/usr/bin/env python3
-"""A simple Flask application that
-supports internationalization."""
-
+"""4-app.y file"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
 class Config:
-    """
-    Configuration class to set up languages and timezone.
-    """
+    """Configuration class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-# Initialize the Flask application
 app = Flask(__name__)
-# Load the configuration from the `Config` class
 app.config.from_object(Config)
-# Initialize Babel with the app for internationalization support
 babel = Babel(app)
 
 
-app.config.from_object('4-app.Config')
-
-
-@babel.localeselector
+# @babel.localeselector
 def get_locale():
-    """
-    Determine the best matching language from supported languages.
-    """
-    locale = request.args.get('locale')
-    if locale and locale in app.config['LANGUAGES']:
-        # Return the locale if it's explicitly specified in the request.
-        return locale
-    # Otherwise, return the best match from the Accept-Language header.
+    """function to get user locale"""
+    force_locale = request.args.get('locale')
+    if force_locale and force_locale in app.config['LANGUAGES']:
+        return force_locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def my_home():
-    """
-    Render the home page of the application.
-    """
-    return render_template('./4-index.html')
+def index():
+    """initial route to index"""
+    # from flask_babel import gettext as _
+    return render_template('4-index.html')
 
 
-if __name__ == "__main__":
-    # This configuration makes the application accessible over the network.
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(debug=True)
