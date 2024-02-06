db24e888f41cc38f84eca25ca0c3ae49a3f57c68
“AAndrews-1982 <altonandrews@icloud.com>
2024-02-06 11:57:57 -0600

    index error

--- MySQL_Advanced/8-index_my_names.sql
+++ MySQL_Advanced/8-index_my_names.sql
@@ -3,4 +3,4 @@ ALTER TABLE names
 ADD COLUMN name_first_letter CHAR(1) AS (LEFT(name, 1)) STORED;
 
 -- Create an index on the generated column
-CREATE INDEX idx_name_first ON names(name_first_letter);
+CREATE INDEX idx_name_first ON names(name(1));
