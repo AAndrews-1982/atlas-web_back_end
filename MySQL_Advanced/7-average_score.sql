034543ca9b8d43279b9cef69383dc73ccc4aa071
“AAndrews-1982 <altonandrews@icloud.com>
2024-02-06 11:36:40 -0600

    Average user

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,15 +1,19 @@
--- Compute and store the average score for a student
+-- TASK7: Enhance the stored procedure to compute and store the average score for a student.
+-- The procedure handles decimal values for precise average score calculations.
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    DECLARE avgScore DECIMAL(10,2);
-
-    -- Calculate the average score for the given user
-    SELECT AVG(score) INTO avgScore FROM scores WHERE user_id = user_id;
-
-    -- Update the user's record with the calculated average score
-    UPDATE users SET average_score = avgScore WHERE id = user_id;
-END //
-
+    -- Directly update the 'average_score' in the 'users' table by calculating
+    -- the average score from the 'corrections' table for the given user.
+    -- The subquery selects the average 'score' where the 'user_id' matches.
+    UPDATE users 
+    SET average_score = (
+        SELECT AVG(score) 
+        FROM corrections 
+        WHERE user_id = user_id
+    )
+    WHERE id = user_id;
+END;
+//
 DELIMITER ;
