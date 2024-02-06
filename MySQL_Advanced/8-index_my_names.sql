d8b72ff9ac888a82fb31fa5ab65402a44e6c6594
“AAndrews-1982 <altonandrews@icloud.com>
2024-02-06 12:41:53 -0600

    Index error fix

--- MySQL_Advanced/8-index_my_names.sql
+++ MySQL_Advanced/8-index_my_names.sql
@@ -1,6 +1,5 @@
 -- First, ensure that any existing index that might conflict is removed
-DROP INDEX IF EXISTS idx_name_first ON names;
+DROP INDEX idx_name_first ON names;
 
 -- Then, create the index on the first letter of the 'name' column
-DROP INDEX idx_name_first ON names;
 CREATE INDEX idx_name_first ON names(name(1));
