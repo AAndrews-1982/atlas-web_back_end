c565799644519cc3d6cf8bda735d9873307ae13c
“AAndrews-1982 <altonandrews@icloud.com>
2024-02-06 11:53:01 -0600

    Optimize simple search

---
+++ MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,6 @@
+-- Add a generated column to 'names' table that stores the first letter of 'name'
+ALTER TABLE names
+ADD COLUMN name_first_letter CHAR(1) AS (LEFT(name, 1)) STORED;
+
+-- Create an index on the generated column
+CREATE INDEX idx_name_first ON names(name_first_letter);
