6a44888fcb02870bcf2a5c007f1c689776999ea4
“AAndrews-1982 <altonandrews@icloud.com>
2024-02-06 12:24:43 -0600

    index error

--- MySQL_Advanced/8-index_my_names.sql
+++ MySQL_Advanced/8-index_my_names.sql
@@ -1,6 +1,5 @@
--- Add a generated column to 'names' table that stores the first letter of 'name'
-ALTER TABLE names
-ADD COLUMN name_first_letter CHAR(1) AS (LEFT(name, 1)) STORED;
+-- First, ensure that any existing index that might conflict is removed
+DROP INDEX IF EXISTS idx_name_first ON names;
 
--- Create an index on the generated column
+-- Then, create the index on the first letter of the 'name' column
 CREATE INDEX idx_name_first ON names(name(1));
