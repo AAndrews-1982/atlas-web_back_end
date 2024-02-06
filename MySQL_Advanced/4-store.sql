e73c3525139d9682d38a28777607626af598147c
“AAndrews-1982 <altonandrews@icloud.com>
2024-02-06 10:51:01 -0600

    added comments

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,3 +1,8 @@
+-- Trigger Name: AfterOrderInsert
+-- Description: Decreases the quantity of an item in the 'items' table after a new order is inserted into the 'orders' table.
+-- Context: This trigger ensures data consistency by automatically updating the items' quantity,
+-- mitigating issues caused by network disconnections, crashes, etc.
+
 DELIMITER $$
 
 CREATE TRIGGER AfterOrderInsert
