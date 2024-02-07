d8660b8e46922912153914558a7478b3d885b249
“AAndrews-1982 <altonandrews@icloud.com>
2024-02-07 10:36:26 -0600

    Drop Function

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,16 +1,23 @@
--- Define the function SafeDiv to perform safe division
+-- Change the delimiter to allow for the function definition in the script
 DELIMITER //
 
-CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS DECIMAL(10,2)
+-- Drop the function if it already exists to avoid errors upon recreation
+DROP FUNCTION IF EXISTS SafeDiv;
+
+-- Create the function SafeDiv
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
 BEGIN
     -- Check if the divisor is zero
     IF b = 0 THEN
-        -- Return 0 if the divisor is zero to avoid division by zero error
+        -- Return 0 if the divisor is zero to avoid division by zero errors
         RETURN 0;
     ELSE
-        -- Perform the division and return the result if the divisor is not zero
+        -- Perform the division if the divisor is not zero
         RETURN a / b;
     END IF;
-END//
+END;
+//
 
+-- Reset the delimiter to its default value
 DELIMITER ;
